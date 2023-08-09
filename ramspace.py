e7035ff49d7e9c56266c1a93c2570db53cb79754
sfluhrer <sfluhrer@cisco.com>
2023-08-09 09:15:25 -0400

    Add comments

--- ramspace.py
+++ ramspace.py
@@ -1,4 +1,18 @@
 #! /usr/bin/env python3
+#
+# This is a script that lists the amount of RAM used by tiny sphincs
+# For each of the supported parameter sets, it compiles a minimal version
+# of tiny sphincs that supports that parameter set, and then runs a utility
+# that probes the RAM usage (for the key generation, signature generation
+# and signature verification) operations
+# It places all these into the ramspace.out file
+#
+# It uses the 'small-and-slow' implementationss of both the SHA2 and SHAKE
+# parameter sets.  Both these options slow down sphincs+ a lot, while saving
+# a relatively small amount of RAM space; the point of this file is to show
+# how little RAM we can use, and so that's what we do.  In practice, we're
+# likely to select the 'use-a-bit-more-ram/go-a-lot-faster' option
+
 import fileinput
 import itertools
 import os
@@ -20,8 +34,14 @@ file1.close()
 for hash in hashes:
     for size in sizes:
         for speed in options:
+            # Defining TUNE_H_ effectively disables the existing tune.h file, and
+            # allows us to plug in our own settings
+            # TS_SHA2_OPTIONIZATION=0, TS_SHAKE256_OPT=2 are the 'small-and-slow'
+            # options for SHA2 and SHAKE256
             DFLAGS = "-DTUNE_H_ -DTS_SHA2_OPTIMIZATION=0"
             DFLAGS = DFLAGS + " -DTS_SHAKE256_OPT=2"
+
+            # Enable the parameter set we're testing
             if size == 256:
                 DFLAGS = DFLAGS + " -DTS_SUPPORT_L5=1 -DTS_SUPPORT_L3=1"
             elif size == 192:
@@ -36,17 +56,25 @@ for hash in hashes:
                 DFLAGS = DFLAGS + " -DTS_SUPPORT_S=1"
             else:
                 DFLAGS = DFLAGS + " -DTS_SUPPORT_S=0"
+
+            # And this is the name of the parameter set
             PARM_SET = 'ts_ps_{}_{}{}_simple'.format(hash, size, speed)
 
+            # Make sure we're running from a clean slate
             run(["make", "clean"], stdout=DEVNULL, stderr=sys.stderr)
 
+            # Compile the package and run the utility that'll generate the
+            # line summarizing this parameter set into the ramspace.out file
             run(["make", "ramspace", 'DFLAGS={}'.format(DFLAGS),
                                      'PARM_SET={}'.format(PARM_SET),
                                      'OUTPUT_FILE=ramspace.out'],
                                      stdout=sys.stdout, stderr=sys.stderr)
 
+            # We've compiled a version of the utility that doesn't correspond to
+            # tune.h - make sure we don't leave anything to trip over
             run(["make", "clean"], stdout=DEVNULL, stderr=sys.stderr)
 
+# And append the explanatory legend at the end
 file1 = open("ramspace.out", "a" );
 file1.write( "\n" );
 file1.write( "Legend:\n" );
